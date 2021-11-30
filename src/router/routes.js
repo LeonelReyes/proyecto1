26c869e310f95f068287543bdacf22e69434c8f4
Leonel Isaac Reyes <72421131+LeonelReyes@users.noreply.github.com>
2021-11-30 19:47:45 -0300

    Add files via upload

---
+++ src/router/routes.js
@@ -0,0 +1,22 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/trabajo', component: () => import('pages/trabajo.vue') },
+      { path: '/personal', component: () => import('pages/personal.vue') },
+      { path: '/universidad', component: () => import('pages/universidad.vue') },
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
